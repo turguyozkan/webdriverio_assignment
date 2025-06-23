c24c042621aa88ed33ecc8259c32d0a4cea3caf0
turgay <nesibeozkan85@gmail.com>
2025-06-23 00:09:24 -0500

    added try catch block

--- test/specs/retirementCalculatorTest.js
+++ test/specs/retirementCalculatorTest.js
@@ -11,33 +11,48 @@ describe("Retirement Calculator Test",async()=>{
     });
     it("Calculate Retirement Savings with social Security Benefits", async () => {
         allure.addTestId('TC-1');
-        await RetirementCalculatorPage.open(); 
-        await RetirementCalculatorPage.closeCookieBannerIfPresent();
-        await RetirementCalculatorPage.fillBasicInfoWssBenefits();
-        await RetirementCalculatorPage.adjustDefaults();
-        await RetirementCalculatorPage.calculateResults();
-        expect(await RetirementCalculatorPage.resultText.getText()).toBe("Results");
+        try{
+            await RetirementCalculatorPage.open(); 
+            await RetirementCalculatorPage.closeCookieBannerIfPresent();
+            await RetirementCalculatorPage.fillBasicInfoWssBenefits();
+            await RetirementCalculatorPage.adjustDefaults();
+            await RetirementCalculatorPage.calculateResults();
+            expect(await RetirementCalculatorPage.resultText.getText()).toBe("Results");
+        }catch(error){
+            allure.addAttachment("Failure", error.message, "text/plain");
+            throw error;
+        }
+        
 
     });
 
     it("Calculate Retirement Savings without social Security Benefits", async () => {
         allure.addTestId('TC-2');
-        await RetirementCalculatorPage.open(); 
-        await RetirementCalculatorPage.closeCookieBannerIfPresent();
-        await RetirementCalculatorPage.fillBasicInfoWssBenefits();
-        await RetirementCalculatorPage.adjustDefaults();
-        await RetirementCalculatorPage.calculateResults();
-        expect(await RetirementCalculatorPage.getResultText()).toBe("Results");
+        try{
+            await RetirementCalculatorPage.open(); 
+            await RetirementCalculatorPage.closeCookieBannerIfPresent();
+            await RetirementCalculatorPage.fillBasicInfoWssBenefits();
+            await RetirementCalculatorPage.adjustDefaults();
+            await RetirementCalculatorPage.calculateResults();
+            expect(await RetirementCalculatorPage.getResultText()).toBe("Results");
+        }catch(error){
+            allure.addAttachment("Failure", error.message, "text/plain");
+            throw error;
+        }
     });
 
     it("Calculate Retirement Savings without a required field", async () => {
         allure.addTestId('TC-3');
-        await RetirementCalculatorPage.open(); 
-        await RetirementCalculatorPage.closeCookieBannerIfPresent();
-        await RetirementCalculatorPage.fillBasicInfoWoRequiredFields();
-        await RetirementCalculatorPage.calculateResults();
-        expect(await RetirementCalculatorPage.getAlertFieldDisplayed({ timeout: 10000 })).toBe(true);
-
+        try{
+            await RetirementCalculatorPage.open(); 
+            await RetirementCalculatorPage.closeCookieBannerIfPresent();
+            await RetirementCalculatorPage.fillBasicInfoWoRequiredFields();
+            await RetirementCalculatorPage.calculateResults();
+            expect(await RetirementCalculatorPage.getAlertFieldDisplayed({ timeout: 10000 })).toBe(true);
+        }catch(error){
+            allure.addAttachment("Failure", error.message, "text/plain");
+            throw error;
+        }
     });
 
 
